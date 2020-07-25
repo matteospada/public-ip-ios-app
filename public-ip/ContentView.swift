11dc8f7c942ec9f7cb759cf49d6fa867e25ab6fc
Matteo Spada <matteo.spada@ymail.com>
2020-07-25 12:29:51 +0200

    Applicazione funzionante e interfaccia grafica completata.

--- public-ip/ContentView.swift
+++ public-ip/ContentView.swift
@@ -7,15 +7,42 @@
 //
 
 import SwiftUI
+import SwiftPublicIP
 
 struct ContentView: View {
+      
+    @State var pubblicIP = "LOADING"
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack
+            {
+                Color(red: 0.22, green: 0.24, blue: 0.27, opacity: 1.00)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack {
+                    Text("YOUR PUBBLIC IP IS:").foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00, opacity: 1.00)).font(.title).padding(10)
+                    
+                     Text(pubblicIP).foregroundColor(Color(red: 1.00, green: 1.00, blue: 1.00, opacity: 1.00)).font(.title).padding(10)
+                    
+                    Text("TAP TO COPY TO CLIPBOARD").foregroundColor(Color(red: 0.77, green: 0.98, blue: 0.43, opacity: 1.00)).font(.footnote).padding(10)
+                    
+                    
+                }
+                // Your other content here
+                // Other layers will respect the safe area edges
+        }.onAppear{
+            SwiftPublicIP.getPublicIP(url: PublicIPAPIURLs.ipv4.icanhazip.rawValue) { (string, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else if let string = string {
+                    self.pubblicIP = string
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone XS"))
     }
 }
